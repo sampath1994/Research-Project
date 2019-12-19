3d29e45c6a1baf19608c5b8133b8c65a113081e1
promod94 <webpromod@gmail.com>
2019-12-19 23:21:46 +0530

    ROI init  mask created

--- speed/geometric_model.py
+++ speed/geometric_model.py
@@ -66,14 +66,48 @@ img = cv2.imread('road_model.jpg', 0)
 ROAD_BOUNDRY_THRESH = 10
 new_img = cv2.imread('background.jpg', cv2.IMREAD_COLOR)
 height, width = img.shape
+left_boundary_img = np.zeros(shape=[height, width, 3], dtype=np.uint8)
+right_boundary_img = np.zeros(shape=[height, width, 3], dtype=np.uint8)
+
 for row in range(height):
     vall, leftB, rightB = score_graph_of_row(img, row, ROAD_BOUNDRY_THRESH)  # 220
     for pnt in vall:
         cv2.circle(new_img, (pnt, row), 1, (0, 0, 255), -1)
     cv2.circle(new_img, (leftB, row), 1, (0, 255, 0), -1)
     cv2.circle(new_img, (rightB, row), 1, (0, 255, 0), -1)
-ret, th = cv2.threshold(img,40,255,cv2.THRESH_BINARY)
+    cv2.circle(left_boundary_img, (leftB, row), 1, (0, 255, 0), -1)
+    cv2.circle(right_boundary_img, (rightB, row), 1, (0, 255, 0), -1)
+ret, th = cv2.threshold(img,20,255,cv2.THRESH_BINARY)
+########################################
+init_mask = np.zeros(shape=[height, width, 1], dtype=np.uint8)
+left_boundary_img = cv2.cvtColor(left_boundary_img, cv2.COLOR_BGR2GRAY)
+right_boundary_img = cv2.cvtColor(right_boundary_img, cv2.COLOR_BGR2GRAY)
+rt, left_th = cv2.threshold(left_boundary_img,10,255,cv2.THRESH_BINARY)
+rt2, right_th = cv2.threshold(right_boundary_img,10,255,cv2.THRESH_BINARY)
+
+for j in range(height - 1):
+    flg = False
+    for i in reversed(range(width - 1)):
+        k = left_boundary_img[j, i]
+        if k > 100:
+            flg = True
+        if flg:
+            init_mask[j, i] = 255
+
+for j in range(height - 1):
+    flg = False
+    for i in range(width - 1):
+        k = right_boundary_img[j, i]
+        if k > 100:
+            flg = True
+        if flg:
+            init_mask[j, i] = 255
+
+########################################
 cv2.imshow('Thresh', th)
 cv2.imshow('Model', new_img)
+cv2.imshow('Left', left_th)
+cv2.imshow('Right', right_th)
+cv2.imshow('Mask', init_mask)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
