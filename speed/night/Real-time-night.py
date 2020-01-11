4a2b14351d73733c98f24785e342102f99774969
promod94 <webpromod@gmail.com>
2020-01-12 00:29:57 +0530

    headlight rules

--- speed/night/Real-time-night.py
+++ speed/night/Real-time-night.py
@@ -2,9 +2,13 @@ import cv2
 import numpy as np
 from pathlib import Path
 from timeit import default_timer as timer
+from speed.night.rules import is_grouped, get_group_bb
 
 video_file = str(Path.cwd().parent.parent / 'videos' / 'CCTV night video.mp4')
 camera = cv2.VideoCapture(video_file)
+
+BB_MIN_HEIGHT = 2
+BB_MIN_WIDTH = 2
 
 while True:
     start = timer()
@@ -14,7 +18,28 @@ while True:
     y, cr, cb = cv2.split(grayvideo)
     blur = cv2.GaussianBlur(y,(5,5),0)
     ret2, th2 = cv2.threshold(blur, 240, 255, cv2.THRESH_BINARY)
-    # th3 = cv2.adaptiveThreshold(y, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 251, 5)
+    im2, contours, hierarchy = cv2.findContours(th2, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    bbs = []
+    for i in range(len(contours)):
+        color_contours = (0, 255, 0)  # green - color for contours
+        color = (255, 0, 0)  # blue - color for convex hull
+        cv2.drawContours(frame, contours, i, color, 1, 8)
+        x, y, w, h = cv2.boundingRect(contours[i])
+        if w > BB_MIN_WIDTH and h > BB_MIN_HEIGHT:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            bbs.append((i, x, y, w, h))
+    bb_len = len(bbs) - 1
+    for idx, bb in enumerate(bbs):
+        if idx != bb_len:
+            for itm in bbs[idx + 1:]:
+                if is_grouped(bb[1:], itm[1:]):
+                    # lx = min(bb[1], itm[1])
+                    # ly = min(bb[2], itm[2])
+                    # rx = max(bb[1], itm[1])
+                    # ry = max(bb[2], itm[2])
+                    tlx, tly, brx, bry = get_group_bb(bb[1:], itm[1:])
+                    cv2.rectangle(frame, (tlx, tly), (brx, bry), (0, 0, 255), 2)
 
     cv2.imshow("video", frame)
     cv2.imshow("converted", th2)
---
+++ speed/night/rules.py
@@ -0,0 +1,57 @@
+def is_grouped(bb1, bb2):
+    x1, y1, w1, h1 = bb1
+    x2, y2, w2, h2 = bb2
+    if dh(bb1, bb2) < 5 * max(h1, h2):
+        if pv(bb1, bb2) > 0.7:
+            if hr(bb1, bb2) > 0.7:
+                return True
+    return False
+
+def dh(bb1, bb2):  # Horizontal distance between BBs
+    x1, _, w1, _ = bb1
+    x2, _, w2, _ = bb2
+    dist = max(x1, x2) - min(x1+w1, x2+w2)
+    return dist
+
+def pv(bb1, bb2):  # overlap between vertical projections of bbs
+    nume = -1 * dv(bb1, bb2)
+    return nume / min(bb1[3], bb2[3])
+
+def dv(bb1, bb2):  # Vertical distance between BBs
+    _, y1, _, h1 = bb1
+    _, y2, _, h2 = bb2
+    return max(y1, y2) - min(y1+h1, y2+h2)
+
+def hr(bb1, bb2):  # height ratio of BBs
+    return min(bb1[3], bb2[3]) / max(bb1[3], bb2[3])
+
+def get_group_bb(bb1 , bb2):
+    tx1, ty1, w1, h1 = bb1
+    tx2, ty2, w2, h2 = bb2
+    bx1 = tx1 + w1
+    by1 = ty1 + h1
+    bx2 = tx2 + w2
+    by2 = ty2 + h2
+    tlx = 0
+    tly = 0
+    brx = 0
+    bry = 0
+    if tx1 < tx2:
+        tlx = tx1
+    else:
+        tlx = tx2
+    if ty1 < ty2:
+        tly = ty1
+    else:
+        tly = ty2
+    if by2 > by1:
+        bry = by2
+    else:
+        bry = by1
+    if bx2 > bx1:
+        brx = bx2
+    else:
+        brx = bx1
+    return tlx, tly, brx, bry
+
+
